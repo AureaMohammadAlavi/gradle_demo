7cc81101dd397457f5228fef4643700341341722
Mohammad Alavi <admin@smash1986.tk>
2020-01-14 22:28:29 +0300

    port

--- web/src/funcTest/groovy/com/manning/gia/todo/web/ToDoHomepage.groovy
+++ web/src/funcTest/groovy/com/manning/gia/todo/web/ToDoHomepage.groovy
@@ -4,6 +4,6 @@ package com.manning.gia.todo.web
 import geb.Page
 
 class ToDoHomepage extends Page {
-    static url = "http://localhost:8080/todo"
+    static url = "http://localhost:8090/todo"
     static at = { title == "To Do application" }
 }
